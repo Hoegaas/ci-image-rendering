2dd801a46f5a70be671ac6f7eb4c9bea5b04dafd
Hoegaas <hoegaas.dev@gmail.com>
2025-06-21 23:28:41 +0200

    peepee

--- python/src/app/is_unrendered_image.py
+++ python/src/app/is_unrendered_image.py
@@ -1,4 +1,17 @@
 import os
-def find_repo_image_files():
-    current_dir = os.getcwd()
-    print("the current repo: " + current_dir)
+import yaml
+#image_extensions = []
+yaml_filename = "galleryze.yaml"
+def read_image_extensions_from_test_yaml(path):
+    with open(path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config.get('image_extensions', [])
+def find_galleryze_yaml():
+    for path in os.getcwd().rglob(yaml_filename):
+        if path is None:
+            print("e bi så ferbaina >:C")
+            return
+        else:
+            return read_image_extensions_from_test_yaml(path)
+image_extensions = find_galleryze_yaml()
+print(image_extensions)
