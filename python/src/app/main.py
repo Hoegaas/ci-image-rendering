29b8c94577bf0c67d487b641e004445bc43405d7
Hoegaas <hoegaas.dev@gmail.com>
2025-06-21 21:56:40 +0200

    42

--- python/src/app/main.py
+++ python/src/app/main.py
@@ -1,8 +1,8 @@
 from yaml_filename_validation import find_file_in_repo
+from is_unrendered_image import find_repo_image_files
 def main():
-    #say_hello("World")
     result = find_file_in_repo()
     print(result)
-
+    find_repo_image_files()
 if __name__ == "__main__":
     main()
