cb502512bfec90f01f96f6feee24639fd6b9c84d
Hoegaas <hoegaas.dev@gmail.com>
2025-06-22 04:31:30 +0200

    peepee

--- python/src/app/is_unrendered_image.py
+++ python/src/app/is_unrendered_image.py
@@ -1,15 +1,37 @@
-import os
-import yaml
+'''
+    A dictionary, mapped pairs of input_image_ext / output_image exts
+        ...This is the on only purposeful data structure for the script...
+    We'll start with identifying any one-to-many pairs..
+        (PNG -> JPG) - straight forward-ish... however:
+        (SVG/PNG -> SVG/PNG/JPEG) involves three modules
+            (1) render SVG -> PNG -> JPEG
+            (2) render the PNG to PNG
+            (3) write the PNG -> SVG
+            (4) write the SVG -> SVG
+'''
+import os, yaml
+from pathlib import Path
 def find_galleryze_yaml(yaml_filename):
     for path in os.getcwd().rglob(yaml_filename):
-        if path is None or len(path) == 0 and path.parent is not os.getcwd():
+        if path is None or len(path) == 0 or path.parent is not os.getcwd():
             return path
         else:
             return None
-def filter_supported_exts(any_image_ext, valid_image_exts):
-    return len([p for p in any_image_ext
-            if p in valid_image_exts])
+def filter_supported_exts(yaml_target_ext, valid_exts):
+    return len([p for p in yaml_target_ext
+            if p in valid_exts])
 def read_image_extensions(parent_path, valid_image_exts):
     with open(parent_path, 'r') as f:
         return filter_supported_exts(yaml.safe_load(f).get(
             'image_extensions', []),valid_image_exts)
+def create_yaml_render_dictionary(filtered_output_exts):
+    return 42
+def load_render_targets(path="galleryze.yaml"):
+    with open(path, "r") as f:
+        data = yaml.safe_load(f)
+    filtered_output_exts = filter_supported_exts(data.get(
+        "render_targets", {}).get("image_extensions", []))
+    dict = create_yaml_render_dictionary(filtered_output_exts)
+def make_render_output_dir(render_output_dir):
+    path = (Path(render_output_dir).mkdir(parents=False, exist_ok=True))
+    print(path)
--- python/src/app/main.py
+++ python/src/app/main.py
@@ -1,11 +1,13 @@
-from is_unrendered_image import find_galleryze_yaml, read_image_extensions
+from is_unrendered_image import find_galleryze_yaml, read_image_extensions, make_render_output_dir, load_render_targets
 valid_image_exts = ['.svg', '.jpeg', '.jpg', '.png']
 #invalid_image_exts_log = []
 yaml_name = "galleryze.yaml/"
+dir_name_renders = "galleryzed-images/"
 yaml_path = find_galleryze_yaml(yaml_name)
-render_output_dir = yaml_path.parent + "galleryzed-images/"
+render_output_dir = yaml_path.parent + dir_name_renders
 valid_image_exts = read_image_extensions(
     yaml_path.parent, valid_image_exts)
+valid_render_target_exts = load_render_targets()
 def main():
     if yaml_path is None:
         print("no file extention in yaml")
@@ -13,7 +15,11 @@ def main():
     if len(valid_image_exts()) == 0:
         print("no supported image exts in repo")
         return
-    print(valid_image_exts)
+    make_render_output_dir(render_output_dir)
+    if len(valid_render_target_exts) == 0:
+        print("no output exts set in yaml")
+        return
+    #print(valid_image_exts)
     print("path: " + yaml_path + 
           "\n parent path: " + yaml_path.parent)
 if __name__ == "__main__":
